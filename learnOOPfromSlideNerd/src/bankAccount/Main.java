903bf8e25073f4e3189b32e2357edd64dd315fdc
AjayGhimire <ajayghimire1998@gmail.com>
2022-12-27 18:05:25 +1100

    bank app continues

--- learnOOPfromSlideNerd/src/bankAccount/Main.java
+++ learnOOPfromSlideNerd/src/bankAccount/Main.java
@@ -9,6 +9,9 @@ public class Main {
 	public static void main(String[] args) {
 
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int numberOfCustomers = 0;
+		Bank bank = new Bank();
+		Customer[] c = bank.getCustomer();
 		while (true) {
 			System.out.println("Please enter your choice: ");
 			System.out.println("1. Add Customer");
@@ -19,10 +22,51 @@ public class Main {
 			System.out.println("6. Exit");
 			try {
 				int choice = Integer.parseInt(bufferedReader.readLine());
+
 				switch (choice) {
+
 				case 1:
+					System.out.println("Creating an account for new customer...");
+					System.out.println("Please enter the initial amount in your account: ");
+					double bal = Double.parseDouble(bufferedReader.readLine());
+					System.out.println("Please enter your account number");
+					String acc = bufferedReader.readLine();
+					Account account = new Account(bal, acc);
+					System.out.println("Please enter your name: ");
+					String name = bufferedReader.readLine();
+					Customer customer = new Customer(name, account);
+					c[numberOfCustomers] = customer;
+					numberOfCustomers++;
+					System.out.println("Number of Customers: " + numberOfCustomers);
+//					for (int i = 0; i < numberOfCustomers; i++) {
+//						System.out.println("Name: " + c[i].getName());
+//					}
 					break;
+
 				case 2:
+					System.out.println("Enter your account number: ");
+					acc = bufferedReader.readLine();
+					if (numberOfCustomers == 0) {
+						System.err.println("Account not found");
+					} else {
+						boolean found = false;
+						for (int i = 0; i < numberOfCustomers; i++) {
+							Account temp = c[i].getAccount();
+							String accTemp = temp.getAccountNumber();
+//							System.out.println(accTemp);
+							if (accTemp.equals(acc)) {
+
+								System.out.println("Please enter the amount to deposit: ");
+								double deposit = Double.parseDouble(bufferedReader.readLine());
+								found = true;
+							}
+
+						}
+						if (found == false) {
+							System.err.println("Account not found");
+						}
+					}
+
 					break;
 				case 3:
 					break;
