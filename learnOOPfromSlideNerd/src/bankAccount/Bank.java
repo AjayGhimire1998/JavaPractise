d0304ecc337a1be5bdb1df67c4d3b23b8e475d3b
AjayGhimire <ajayghimire1998@gmail.com>
2022-12-27 17:45:03 +1100

    bank app continued

--- learnOOPfromSlideNerd/src/bankAccount/Bank.java
+++ learnOOPfromSlideNerd/src/bankAccount/Bank.java
@@ -5,8 +5,13 @@ public class Bank {
 	private double TRANSACTION_FEE = 10;
 	private Customer[] customers = new Customer[1000];
 
-	public void calculateInterest() {
-
+	public void calculateInterest(Customer customer) {
+		Account a = customer.getAccount();
+		double bal = a.getBalance();
+		double interestAmount = bal * INTEREST_RATE / 100;
+		double totalBalance = bal + interestAmount;
+		System.out.println("Interest amount: " + interestAmount);
+		System.out.println("Total balace: " + totalBalance);
 	}
 
 	public double getInterestRate() {
@@ -15,6 +20,10 @@ public class Bank {
 
 	public double getTransactionFee() {
 		return TRANSACTION_FEE;
+	}
+
+	public Customer[] getCustomer() {
+		return customers;
 	}
 
 }
--- learnOOPfromSlideNerd/src/bankAccount/Main.java
+++ learnOOPfromSlideNerd/src/bankAccount/Main.java
@@ -1,9 +1,50 @@
 package bankAccount;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			System.out.println("Please enter your choice: ");
+			System.out.println("1. Add Customer");
+			System.out.println("2. Deposit Money");
+			System.out.println("3. Withdraw Money");
+			System.out.println("4. Check Balance");
+			System.out.println("5. Calculate Interest");
+			System.out.println("6. Exit");
+			try {
+				int choice = Integer.parseInt(bufferedReader.readLine());
+				switch (choice) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					System.exit(0);
+					break;
+				default:
+					break;
+				}
+			} catch (NumberFormatException e) {
+
+				e.printStackTrace();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 }
