ec754d9ec25f1862e46d754feb21435c7a868e60
AjayGhimire <ajayghimire1998@gmail.com>
2022-12-28 14:48:32 +1100

    reference preservation and sttaic variables

--- learnOOPfromSlideNerd/src/thisKeyword/Main.java
+++ learnOOPfromSlideNerd/src/thisKeyword/Main.java
@@ -7,14 +7,26 @@ public class Main {
 		Test test1 = new Test();
 //		Test test2 = new Test();
 
-		System.out.println(test1.id);
+//		System.out.println(test1.id);
 //		System.out.println(test2.id);
 
 		Test test2 = new Test();
-		System.out.println(test2.id);
+//		System.out.println(test2.id);
 
 		Test test3 = new Test();
-		System.out.println(test3.id);
+//		System.out.println(test3.id);
+//		System.out.println(Test.id);
+//		System.out.println(Test.myConditiob);
+//		System.out.println(Test.c);
+//		System.out.println(Test.obj);
+
+		// == vs euals
+
+		String s1 = new String("hello");
+		String s2 = new String("hello");
+
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
 
 	}
 
@@ -22,8 +34,21 @@ public class Main {
 
 class Test {
 	static int id = 1000;
+	static boolean myConditiob = true;
+	static char c = 'A';
+	static Object obj = new Object();
+	static int uId;
+	static {
+		uId = 1001;
+		System.out.println("Look I am first");
+	}
+	static {
+		uId = 10000;
+		System.out.println("Look I am second");
+	}
 
 	Test() {
-		id++;
+//		id++;
+		System.out.println("Contructors called");
 	}
 }
