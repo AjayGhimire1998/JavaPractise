c28e8678dbcb32c7109bc1f56727a0275b9a7dfa
AjayGhimire <ajayghimire1998@gmail.com>
2022-12-29 00:16:01 +1100

    started inheritance

---
+++ learnOOPfromSlideNerd/src/inheritance/Cycle.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Cycle {
+	int weight = 10;
+
+	public int getWeight() {
+		return weight;
+	}
+
+}
--- learnOOPfromSlideNerd/src/inheritance/Main.java
+++ learnOOPfromSlideNerd/src/inheritance/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Cycle cycle = new Cycle();
+
+		Bicycle bicycle = new Bicycle();
+
+		bicycle.getInfo();
+//		bicycle.getWeight();
 	}
 
 }
