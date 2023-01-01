ea117326ca98fea5bb933960c2998ceb1f92b169
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-01 12:09:54 +1100

    overloaded methods

--- FirstProgram/src/Main.java
+++ FirstProgram/src/Main.java
@@ -352,7 +352,7 @@ public class Main {
 		// methods : a block of code executed on called upon
 //		hello("Ajay", 34);
 
-//		int x = 3;
+///		int x = 3;
 //		int y = 6;
 //		System.out.println(sumOfTwoNumbers(x, y));
 
@@ -415,34 +415,34 @@ public class Main {
 	// overloaded methods: methods that share the same name but have different
 	// parameters, method name + parameters = method signature
 
-//	static int add(int a, int b) {
-//		System.out.println("OverLoaded method 1");
-//		return a + b;
-//	}
-//
-//	static int add(int a, int b, int c) {
-//		System.out.println("OverLoaded method 2");
-//		return a + b + c;
-//	}
-//
-//	static int add(int a, int b, int c, int d) {
-//		System.out.println("OverLoaded method 3");
-//		return a + b + c + d;
-//	}
-//
-//	static double add(double a, double b) {
-//		System.out.println("OverLoaded method 4");
-//		return a + b;
-//	}
-//
-//	static double add(double a, double b, double c) {
-//		System.out.println("OverLoaded method 5");
-//		return a + b + c;
-//	}
-//
-//	static double add(double a, double b, double c, double d) {
-//		System.out.println("OverLoaded method 6");
-//		return a + b + c + d;
-//	}
+	static int add(int a, int b) {
+		System.out.println("OverLoaded method 1");
+		return a + b;
+	}
+
+	static int add(int a, int b, int c) {
+		System.out.println("OverLoaded method 2");
+		return a + b + c;
+	}
+
+	static int add(int a, int b, int c, int d) {
+		System.out.println("OverLoaded method 3");
+		return a + b + c + d;
+	}
+
+	static double add(double a, double b) {
+		System.out.println("OverLoaded method 4");
+		return a + b;
+	}
+
+	static double add(double a, double b, double c) {
+		System.out.println("OverLoaded method 5");
+		return a + b + c;
+	}
+
+	static double add(double a, double b, double c, double d) {
+		System.out.println("OverLoaded method 6");
+		return a + b + c + d;
+	}
 
 }
