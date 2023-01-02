13c9ec5578a3d784416e4c62fba7cbc633e8c5fa
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-02 17:15:03 +1100

    abstract classes parctise

--- learnOOPfromSlideNerd/src/abstractClass/Main.java
+++ learnOOPfromSlideNerd/src/abstractClass/Main.java
@@ -1,8 +1,53 @@
 package abstractClass;
 
+;
+
 public class Main {
 
 	public static void main(String[] args) {
+//		SuperClass superClass = new SuperClass();
+//		SubClass subClass = new SubClass();
+
+		Test test = new Test();
+
+		test.sayHello();
+		test.printInfo();
+		System.out.println(test.name);
+
+	}
+
+}
+
+abstract class SuperClass {
+	String name = "MySuperClass";
+
+	public void sayHello() {
+		System.out.println("Hello");
+	}
+
+	abstract public void printInfo();
+}
+
+abstract class SubClass extends SuperClass {
+	@Override
+	public void printInfo() {
+		System.out.println("I am inside the subclass");
+	}
+
+	abstract public void sayHello();
+}
+
+class Test extends SubClass {
+
+	@Override
+	public void sayHello() {
+
+	}
+
+	@Override
+	public void printInfo() {
+		// TODO Auto-generated method stub
+		System.out.println("I am test");
 
 	}
 
