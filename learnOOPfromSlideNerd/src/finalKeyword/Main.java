a9e812ac11b3dae5a197d8fa3a552880ef73310a
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-02 18:03:58 +1100

    final variables

---
+++ learnOOPfromSlideNerd/src/finalKeyword/Main.java
@@ -0,0 +1,23 @@
+package finalKeyword;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// constant
+		final double PI = 3.14;
+		PI = 4.0; // invalid, final variable PI cannot be reassigned
+
+		// object with changeable state
+		final ArrayList<String> planets = new ArrayList<>();
+		planets.add("Mercury"); // valid
+		planets.add("Venus"); // valid
+		planets.add("Earth"); // valid
+		planets.remove(0); // valid
+		planets = new ArrayList<>(); // invalid, the planets variable
+										// itself cannot be reassigned to
+										// reference a different object.
+	}
+
+}
