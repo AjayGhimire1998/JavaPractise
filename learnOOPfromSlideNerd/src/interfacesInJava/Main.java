c93f6637b4253650c8ba41d17a28bb7bc0754a3e
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-02 20:29:37 +1100

    interface

--- learnOOPfromSlideNerd/src/interfacesInJava/Main.java
+++ learnOOPfromSlideNerd/src/interfacesInJava/Main.java
@@ -1,10 +1,36 @@
 package interfacesInJava;
 
-public class Main {
+public class Main implements myInterface, myInterface2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main main = new Main();
+		main.display();
+		main.test();
+	}
+
+	@Override
+	public void display() {
+//		PI = 2.0f;
+		System.out.println("Hi from interface: " + PI);
 
 	}
+
+	@Override
+	public void test() {
+		System.out.println("testing interface");
+
+	}
+
+}
+
+interface myInterface {
+	float PI = 3.13f;
+
+	void display();
+
+}
+
+interface myInterface2 {
+	void test();
 
 }
