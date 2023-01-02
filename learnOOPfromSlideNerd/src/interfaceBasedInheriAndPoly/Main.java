c57b6774ba139b0c7463511b51cf3bfa0a848136
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-02 21:21:10 +1100

    interface polymorphism practise

--- learnOOPfromSlideNerd/src/interfaceBasedInheriAndPoly/Main.java
+++ learnOOPfromSlideNerd/src/interfaceBasedInheriAndPoly/Main.java
@@ -9,9 +9,28 @@ public class Main {
 	public static void main(String[] args) {
 //		Measurable measurable = new Reactangle(10, 20); // rectangle implements Measurable
 
-		Reactangle rect1 = new Reactangle(10, 20);
-		Circle circle1 = new Circle(10);
+		Measurable rect1 = new Reactangle(10, 20);
+		Measurable circle1 = new Circle(10);
 		addArea(rect1, circle1);
+
+		Measurable r1 = new Reactangle(10, 10);
+		Reactangle r2 = new Reactangle(10, 10);
+
+		Reactangle r3 = (Reactangle) r1; // measureable cannot be converted to the rectangle
+
+		addArea(r2, r3);
+
+		Measurable r4 = new Circle(10);
+		Reactangle r5 = new Reactangle(10, 10);
+
+		if (r4 instanceof Circle) {
+			System.out.println("It is a cicle");
+		}
+		if (r4 instanceof Reactangle) {
+			Reactangle rect = (Reactangle) r4;
+			rect.hello();
+
+		}
 
 	}
 
@@ -49,6 +68,9 @@ class Reactangle implements Measurable {
 		return length * breadth;
 	}
 
+	public void hello() {
+		System.out.println("HELLO FROM RECT");
+	}
 }
 
 class Circle implements Measurable {
