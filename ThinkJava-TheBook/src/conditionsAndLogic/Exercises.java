8ba03f2cf602b22a027c1dec2bc3f20039f6d462
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-27 20:17:32 +1100

    guess my Number started

--- ThinkJava-TheBook/src/conditionsAndLogic/Exercises.java
+++ ThinkJava-TheBook/src/conditionsAndLogic/Exercises.java
@@ -57,4 +57,19 @@ public class Exercises {
 		}
 
 	}
+
+	/**
+	 * Now that we have conditional statements, we can get back to the “Guess My
+	 * Number” game from Exercise 3-4. You should already have a program that
+	 * chooses a random number, prompts the user to guess it, and displays the
+	 * difference between the guess and the chosen number. Adding a small amount of
+	 * code at a time, and testing as you go, modify the program so it tells the
+	 * user whether the guess is too high or too low, and then prompts the user for
+	 * another guess. The program should continue until the user gets it right.
+	 * Hint: Use two methods, and make one of them recursive.
+	 */
+
+	public static void guessMyNumber() {
+
+	}
 }
