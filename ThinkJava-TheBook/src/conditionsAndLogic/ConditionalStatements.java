29cb6384c526a431c0c0a5e4e14b6723bff9b0a2
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-27 18:33:45 +1100

    switch case

--- ThinkJava-TheBook/src/conditionsAndLogic/ConditionalStatements.java
+++ ThinkJava-TheBook/src/conditionsAndLogic/ConditionalStatements.java
@@ -12,9 +12,22 @@ public class ConditionalStatements {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the integer you want to check: ");
 		int input = in.nextInt();
-		if (input % 2 == 0)
-			System.out.println("It is even !");
-		else
-			System.out.println("It is odd!");
+//		if (input % 2 == 0)
+//			System.out.println("It is even !");
+//		else
+//			System.out.println("It is odd!");
+
+		switch (input % 2) {
+		case 0:
+			System.out.println("It is even");
+			break;
+		case 1:
+		case -1:
+			System.out.println("it is odd");
+			break;
+		default:
+			System.out.println("Invalid input");
+			break;
+		}
 	}
 }
