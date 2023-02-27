58c541285af5c30844a77673d9abebd370f9613c
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-27 19:46:54 +1100

    converting int to binary and hex using recurssion

--- ThinkJava-TheBook/src/conditionsAndLogic/RecursiveMethods.java
+++ ThinkJava-TheBook/src/conditionsAndLogic/RecursiveMethods.java
@@ -4,7 +4,11 @@ public class RecursiveMethods {
 
 	public static void main(String[] args) {
 //		countdownWithParams(5);
-		countUp(6);
+//		countUp(6);
+//		displayBinary(23);
+		displayHex(20);
+		System.out.println();
+
 	}
 
 	public static void countdownWithParams(int num) {
@@ -26,4 +30,18 @@ public class RecursiveMethods {
 
 	}
 
+	public static void displayBinary(int num) {
+		if (num > 0) {
+			displayBinary(num / 2);
+			System.out.print(num % 2);
+		}
+
+	}
+
+	public static void displayHex(int num) {
+		if (num > 0) {
+			displayHex(num / 16);
+			System.out.print(num % 16);
+		}
+	}
 }
