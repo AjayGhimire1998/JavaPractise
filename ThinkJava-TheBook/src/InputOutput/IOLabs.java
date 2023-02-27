6c7c060dff336753b690702d9f82c8fa319e6755
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-27 17:21:53 +1100

    guessing game

--- ThinkJava-TheBook/src/InputOutput/IOLabs.java
+++ ThinkJava-TheBook/src/InputOutput/IOLabs.java
@@ -1,14 +1,16 @@
 package InputOutput;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class CelciusToFaraheinheit {
+public class IOLabs {
 	final static double FAR_PER_CEL = 9.0 / 5.0;
 	final static double FAR_PER_ZERO_CEL = 32.0;
 
 	public static void main(String[] args) {
 //		celToFar();
-		secondsToHoursMinutesSeconds();
+//		secondsToHoursMinutesSeconds();
+		guessStarter();
 	}
 
 	static void celToFar() {
@@ -31,9 +33,29 @@ public class CelciusToFaraheinheit {
 		hours = minutes / 60;
 		minutes = minutes % 60;
 
-		System.out.println(minutes);
 		System.out.printf("%d hours, %d minutes, %d seconds", hours, minutes, remainder);
-
 	}
 
+	static void guessStarter() {
+
+		Random rand = new Random();
+		int num = rand.nextInt(100) + 1;
+
+		System.out.println(
+				"I'm thinking of a number between 1 and 100\n" + "(including both). Can you guess what it is?");
+		System.out.print("Type a number: ");
+		Scanner in = new Scanner(System.in);
+		int guess = in.nextInt();
+
+		System.out.println("Your guess is: " + guess);
+
+		System.out.println("The number I was thinking is: " + num);
+
+		if (guess > num) {
+			System.out.println("you were off by: " + (guess - num));
+		} else {
+			System.out.println("you were off by: " + (num - guess));
+		}
+
+	}
 }
