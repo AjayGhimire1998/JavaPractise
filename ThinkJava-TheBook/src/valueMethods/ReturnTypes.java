83398365cff68afd1a26ec248f9d833b53d3e0ee
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-28 20:36:51 +1100

    circle radius returns

--- ThinkJava-TheBook/src/valueMethods/ReturnTypes.java
+++ ThinkJava-TheBook/src/valueMethods/ReturnTypes.java
@@ -9,12 +9,17 @@ public class ReturnTypes {
 //		System.out.println(print);
 
 		System.out.println(calculateArea(2.0));
+		System.out.printf("%.2f square radius", calculateAreaConcise(2.0));
 
 	}
 
 	public static double calculateArea(double radius) {
 		double result = Math.PI * radius * radius;
 		return result;
+	}
+
+	public static double calculateAreaConcise(double radius) {
+		return Math.PI * radius * radius;
 	}
 
 }
