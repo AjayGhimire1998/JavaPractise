368fbacd2940b3de56936ccacead50efcc3d06b7
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-28 20:33:28 +1100

    value methods started

--- ThinkJava-TheBook/src/valueMethods/ReturnTypes.java
+++ ThinkJava-TheBook/src/valueMethods/ReturnTypes.java
@@ -5,6 +5,10 @@ public class ReturnTypes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		double print = calculateArea(2.0);
+
+//		System.out.println(print);
+
 	}
 
 	public static double calculateArea(double radius) {
