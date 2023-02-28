db2aecbefb191911b9b27f72ac9f9157d5328ee6
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-28 20:14:09 +1100

    guess my Number started

--- ThinkJava-TheBook/src/conditionsAndLogic/Exercises.java
+++ ThinkJava-TheBook/src/conditionsAndLogic/Exercises.java
@@ -1,5 +1,8 @@
 package conditionsAndLogic;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercises {
 
 	public static void main(String[] args) {
@@ -70,6 +73,14 @@ public class Exercises {
 	 */
 
 	public static void guessMyNumber() {
+		Random random = new Random();
+		int rand = random.nextInt(100) + 1;
+
+		System.out.println(
+				"I'm thinking of a number between 1 and 100\n" + "(including both). Can you guess what it is?");
+		System.out.print("Type a number: ");
+		Scanner in = new Scanner(System.in);
+		int guess = in.nextInt();
 
 	}
 
