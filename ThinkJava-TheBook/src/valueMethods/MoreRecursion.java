e17c078bd08c7217d0eeb686efb0764f78fba447
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-01 21:42:19 +1100

    more recursion factorial

--- ThinkJava-TheBook/src/valueMethods/MoreRecursion.java
+++ ThinkJava-TheBook/src/valueMethods/MoreRecursion.java
@@ -3,17 +3,23 @@ package valueMethods;
 public class MoreRecursion {
 
 	public static void main(String[] args) {
-
+		int print = factorial(5);
+		System.out.println(print);
 	}
 
 	/**
 	 * factorial method to return the factorial of given integer
 	 * 
 	 * @param n, which is an integer
-	 * @return
+	 * @return 1 if n is 0, or else cursively factorise
 	 */
 	public static int factorial(int n) {
-		return 0;
+		if (n == 0) {
+			return 1;
+		}
+		int recurse = factorial(n - 1);
+		int result = n * recurse;
+		return result;
 	}
 
 }
