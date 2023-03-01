3fe93d82d83440be4e31c5652a15cea56491c017
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-01 22:09:00 +1100

    isDivisible done

--- ThinkJava-TheBook/src/valueMethods/Labs.java
+++ ThinkJava-TheBook/src/valueMethods/Labs.java
@@ -2,9 +2,12 @@ package valueMethods;
 
 public class Labs {
 	public static void main(String[] args) {
-
+		boolean result = isDivisible(8, 2);
+		System.out.println(result);
 	}
 
-//	public static 
+	public static boolean isDivisible(int n, int m) {
+		return n % m == 0;
+	}
 
 }
