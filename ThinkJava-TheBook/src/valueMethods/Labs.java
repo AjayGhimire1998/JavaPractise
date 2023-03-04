caca08f7f5fb9cd14bbb95078dd9a998d102dc8b
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-04 15:54:45 +1100

    exercise 7 started

--- ThinkJava-TheBook/src/valueMethods/Labs.java
+++ ThinkJava-TheBook/src/valueMethods/Labs.java
@@ -104,4 +104,18 @@ public class Labs {
 		}
 	}
 
+	/**
+	 * Write a recursive method named oddSum that takes a positive odd integer n and
+	 * returns the sum of odd integers from 1 to n. Start with a base case, and use
+	 * temporary variables to debug your solution. You might find it helpful to
+	 * print the value of n each time oddSum is invoked.
+	 * 
+	 * @param p
+	 * @return
+	 */
+	public static int oddSum(int p) {
+
+		return p;
+	}
+
 }
