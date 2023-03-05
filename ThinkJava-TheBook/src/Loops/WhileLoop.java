d1c10a474929fab6e040d0f773d6aaac238e9b35
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-05 16:56:11 +1100

    while loop

--- ThinkJava-TheBook/src/Loops/WhileLoop.java
+++ ThinkJava-TheBook/src/Loops/WhileLoop.java
@@ -4,6 +4,16 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		countDown(10);
+
+	}
+
+	public static void countDown(int n) {
+		while (n > 0) {
+			System.out.println(n);
+			n--;
+		}
+		System.out.println("Boom");
 
 	}
 
