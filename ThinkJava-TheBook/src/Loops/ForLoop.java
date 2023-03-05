43e360e8c30437419744d1a085e28d71f19a2a44
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-05 17:41:47 +1100

    for loop done

--- ThinkJava-TheBook/src/Loops/ForLoop.java
+++ ThinkJava-TheBook/src/Loops/ForLoop.java
@@ -17,8 +17,25 @@ public class ForLoop {
 	 */
 	public static void printTable(int rows) {
 		for (int i = 1; i <= rows; i++) {
-			EncapAndGeneralisation.printRow(i, rows);
+//			EncapAndGeneralisation.printRow(i, rows);
+			printRow(i, rows);
 		}
+	}
+
+	/**
+	 * The last line tries to display i (for no reason other than demonstration) but
+	 * it won’t work. If you need to use a loop variable outside the loop, you have
+	 * to declare it out‐ side the loop, like this:
+	 * 
+	 * @param n
+	 * @param cols
+	 */
+	public static void printRow(int n, int cols) {
+		int i;
+		for (i = 1; i <= cols; i++) {
+			System.out.printf("%4d", n * i);
+		}
+		System.out.println();
 	}
 
 }
