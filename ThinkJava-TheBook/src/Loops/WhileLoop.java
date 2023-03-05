05bc50f1569a04c0cc4ee659893d0eaa3b2bf6a0
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-05 17:02:25 +1100

    does while lop always terminates?

--- ThinkJava-TheBook/src/Loops/WhileLoop.java
+++ ThinkJava-TheBook/src/Loops/WhileLoop.java
@@ -4,7 +4,8 @@ public class WhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		countDown(10);
+//		countDown(10);
+		sequence(97);
 
 	}
 
@@ -15,6 +16,17 @@ public class WhileLoop {
 		}
 		System.out.println("Boom");
 
+	}
+
+	public static void sequence(int n) {
+		while (n != 1) {
+			System.out.println(n);
+			if (n % 2 == 0) {
+				n = n / 2;
+			} else {
+				n = n * 3 + 1;
+			}
+		}
 	}
 
 }
