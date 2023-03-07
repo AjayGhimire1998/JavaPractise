0640de4593e0be7106ef28ca1ca5a202d5140924
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-07 23:22:56 +1100

    Inout Output done

--- ThinkJava-TheBook/src/InputOutput/IOLabsRevision.java
+++ ThinkJava-TheBook/src/InputOutput/IOLabsRevision.java
@@ -1,12 +1,14 @@
 package InputOutput;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class IOLabsRevision {
 
 	public static void main(String[] args) {
 //		celToFarh();
-		secToHourMinSec();
+//		secToHourMinSec();
+		guessStarter();
 	}
 
 	public static void celToFarh() {
@@ -29,6 +31,20 @@ public class IOLabsRevision {
 		int hour = actualMinutes / 60;
 
 		System.out.printf("%d seconds = %d hours, %d minutes, %d seconds", sec, hour, minutes, seconds);
+	}
+
+	public static void guessStarter() {
+		Random rand = new Random();
+		int randGuess = rand.nextInt(100) + 1;
+		System.out.println(
+				"I'm thinking of a number between 1 and 100\n" + "(including both). Can you guess what it is?");
+		Scanner in = new Scanner(System.in);
+		System.out.println("type your guess: ");
+		int yourGuess = in.nextInt();
+		System.out.println("your guess is: " + yourGuess);
+		System.out.println("the number I was thinking is: " + randGuess);
+		System.out.println(
+				"you were off by: " + (randGuess > yourGuess ? (randGuess - yourGuess) : (yourGuess - randGuess)));
 	}
 
 }
