374ed082cb17c8736ab7413429e7a99810f57e3f
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-08 20:31:22 +1100

    power revised

--- ThinkJava-TheBook/src/valueMethods/ValueMethodsLabRevision.java
+++ ThinkJava-TheBook/src/valueMethods/ValueMethodsLabRevision.java
@@ -10,6 +10,8 @@ public class ValueMethodsLabRevision {
 		System.out.println(isTriangle(2, 1, 12));
 		System.out.println(multAdd(1, 2, 3));
 		System.out.println(oddSum(13));
+		System.out.println(ack(3, 5));
+		System.out.println(power(2, 5));
 
 	}
 
@@ -42,6 +44,24 @@ public class ValueMethodsLabRevision {
 			int hehe = a;
 			hehe += oddSum(a - 2);
 			return hehe;
+		}
+	}
+
+	public static int ack(int m, int n) {
+		if (m == 0) {
+			return n + 1;
+		} else if (m > 0 && n == 0) {
+			return ack(m - 1, n);
+		} else {
+			return ack(m - 1, ack(m, n - 1));
+		}
+	}
+
+	public static double power(double x, int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return x * power(x, n - 1);
 		}
 	}
 
