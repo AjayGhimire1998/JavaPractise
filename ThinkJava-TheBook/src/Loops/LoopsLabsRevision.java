a6361cc19fd66fe7f354aa33bdd8f937ec2b4b69
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-08 20:56:05 +1100

    myExp revised

--- ThinkJava-TheBook/src/Loops/LoopsLabsRevision.java
+++ ThinkJava-TheBook/src/Loops/LoopsLabsRevision.java
@@ -5,7 +5,9 @@ public class LoopsLabsRevision {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(squareRoot(16.0));
-		System.out.println(power(2.0, 5));
+		System.out.println(power(2.0, 0));
+		System.out.println(factorial(7));
+		System.out.println(myExp(2, 3));
 	}
 
 	public static double squareRoot(double x) {
@@ -24,6 +26,22 @@ public class LoopsLabsRevision {
 		double result = 1;
 		for (int i = 1; i <= n; i++) {
 			result *= x;
+		}
+		return result;
+	}
+
+	public static int factorial(int n) {
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+	}
+
+	public static double myExp(double x, int n) {
+		int result = 0;
+		for (int i = 0; i <= n; i++) {
+			result += power(x, i) / factorial(i);
 		}
 		return result;
 	}
