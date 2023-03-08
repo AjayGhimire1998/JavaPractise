48445ff2818635f796f25a56db695af4b859947f
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-08 21:37:05 +1100

    finding max vakue in an array revised

--- ThinkJava-TheBook/src/arrays/ArraysLabsRevision.java
+++ ThinkJava-TheBook/src/arrays/ArraysLabsRevision.java
@@ -7,10 +7,14 @@ public class ArraysLabsRevision {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double[] test = { 1, 2, 3, 4, 5 };
+		int[] test3 = { 1, 2, 3, 4, 5 };
 		int[] test2 = { 1, 2, 4, 5, 6, 9, 9, 1, 7, 2, 99, 98, 99 };
 		System.out.println(Arrays.toString(powArray(test)));
 		System.out.println(Arrays.toString(powArrayTwo(test, 3)));
 		System.out.println(Arrays.toString(histogram(test2)));
+		System.out.println(maxValue(test2));
+		System.out.println(maxValue(test3));
+//		System.out.println(indexOfMax(test2));
 	}
 
 	public static double[] powArray(double[] a) {
@@ -42,6 +46,20 @@ public class ArraysLabsRevision {
 			test[count]++;
 		}
 		return test;
+	}
+
+	public static int maxValue(int[] a) {
+		int maxValue = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] > maxValue) {
+				maxValue = a[i];
+			}
+		}
+		return maxValue;
+	}
+
+	public static int indexOfMax() {
+
 	}
 
 }
