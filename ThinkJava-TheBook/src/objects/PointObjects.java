4d61d115c4a5d15960d21dd580a9f53124421c96
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-09 20:07:16 +1100

    point objects

--- ThinkJava-TheBook/src/objects/PointObjects.java
+++ ThinkJava-TheBook/src/objects/PointObjects.java
@@ -1,10 +1,16 @@
 package objects;
 
+import java.awt.Point;
+
 public class PointObjects {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Point blank;
+		blank = new Point(3, 4);
 
+		int x = blank.x;
+		System.out.println(x);
 	}
 
 }
