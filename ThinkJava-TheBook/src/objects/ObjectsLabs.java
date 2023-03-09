333a28b49cacc30269f58cdee802c955e0e9ed37
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-09 21:11:40 +1100

    written factorial for big data ints and created tables

--- ThinkJava-TheBook/src/objects/ObjectsLabs.java
+++ ThinkJava-TheBook/src/objects/ObjectsLabs.java
@@ -1,15 +1,25 @@
 package objects;
 
+import java.math.BigInteger;
+
 public class ObjectsLabs {
 	public static void main(String[] args) {
-		System.out.println(factorial(7));
+//		System.out.println(factorial(17));
+		tableOfFactorials();
 	}
 
-	public static int factorial(int n) {
-		int result = 1;
+	public static BigInteger factorial(int n) {
+		BigInteger result = BigInteger.ONE;
 		for (int i = 1; i <= n; i++) {
-			result *= i;
+			result = result.multiply(BigInteger.valueOf(i));
 		}
 		return result;
+	}
+
+	public static void tableOfFactorials() {
+		for (int i = 0; i <= 30; i++) {
+
+			System.out.println(i + "      " + factorial(i));
+		}
 	}
 }
