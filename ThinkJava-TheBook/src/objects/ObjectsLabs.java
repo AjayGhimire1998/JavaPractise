e93645d244bd128a7f23a9e4663f772ef94e06bf
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-09 20:58:43 +1100

    factorial again

--- ThinkJava-TheBook/src/objects/ObjectsLabs.java
+++ ThinkJava-TheBook/src/objects/ObjectsLabs.java
@@ -2,6 +2,14 @@ package objects;
 
 public class ObjectsLabs {
 	public static void main(String[] args) {
+		System.out.println(factorial(7));
+	}
 
+	public static int factorial(int n) {
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
 	}
 }
