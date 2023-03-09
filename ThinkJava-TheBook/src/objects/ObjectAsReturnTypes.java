fee1d46011f2bec0f19d27e5b59abfd8bf21b821
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-09 20:15:33 +1100

    java default Rectangle class

--- ThinkJava-TheBook/src/objects/ObjectAsReturnTypes.java
+++ ThinkJava-TheBook/src/objects/ObjectAsReturnTypes.java
@@ -1,10 +1,13 @@
 package objects;
 
+import java.awt.Rectangle;
+
 public class ObjectAsReturnTypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Rectangle box = new Rectangle(0, 0, 100, 200);
+		System.out.println(box);
 	}
 
 }
