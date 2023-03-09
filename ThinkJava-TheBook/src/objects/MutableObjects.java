d45e30e1503ac2a3c310b4b1b450372e942284b6
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-09 20:24:49 +1100

    translate method provided by thr rectangle class

--- ThinkJava-TheBook/src/objects/MutableObjects.java
+++ ThinkJava-TheBook/src/objects/MutableObjects.java
@@ -13,8 +13,22 @@ public class MutableObjects {
 		moveRect(box, 20, 30);
 		System.out.println(box);
 
+		// or
+
+		box.translate(10, 10);
+		System.out.println(box);
+
 	}
 
+	/**
+	 * The variables dx and dy indicate how far to move the rectangle in each
+	 * direction. Invoking this method has the effect of modifying the Rectangle
+	 * that is passed as an argument.
+	 * 
+	 * @param rect
+	 * @param dx
+	 * @param dy
+	 */
 	public static void moveRect(Rectangle rect, int dx, int dy) {
 		rect.x = rect.x + dx;
 		rect.y = rect.y + dy;
--- ThinkJava-TheBook/src/objects/ObjectAsReturnTypes.java
+++ ThinkJava-TheBook/src/objects/ObjectAsReturnTypes.java
@@ -12,6 +12,17 @@ public class ObjectAsReturnTypes {
 		System.out.println(findCenter(box));
 	}
 
+	/**
+	 * You can write methods that return objects. For example, findCenter takes a
+	 * Rectangle as an argument and returns a Point with the coordinates of the
+	 * center of the rectangle:
+	 * 
+	 * The return type of this method is Point. The last line creates a new Point
+	 * object and returns a reference to it.
+	 * 
+	 * @param rect
+	 * @return
+	 */
 	public static Point findCenter(Rectangle rect) {
 		int x = rect.x + rect.width / 2;
 		int y = rect.y + rect.height / 2;
