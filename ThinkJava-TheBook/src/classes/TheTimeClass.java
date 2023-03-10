a7d0949b44e1f04d7e3e3d6e21e36471116f729d
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-10 18:16:09 +1100

    created overloaded constructors and printed values to string

--- ThinkJava-TheBook/src/classes/TheTimeClass.java
+++ ThinkJava-TheBook/src/classes/TheTimeClass.java
@@ -2,8 +2,37 @@ package classes;
 
 public class TheTimeClass {
 
+	public static void main(String[] args) {
+		TheTimeClass time1 = new TheTimeClass(2);
+		System.out.println(time1.hour);
+		time1.printValues();
+	}
+
 	private int hour;
 	private int minute;
 	private double second;
+
+	public TheTimeClass() {
+		this(0, 0, 0);
+	}
+
+	public TheTimeClass(int hour) {
+		this(hour, 0, 0);
+	}
+
+	public TheTimeClass(int hour, int minute) {
+		this(hour, minute, 0);
+	}
+
+	public TheTimeClass(int hour, int minute, int second) {
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+
+	public void printValues() {
+		System.out.println(this.hour + " hour : " + this.minute + " minute : " + this.second + " second");
+
+	}
 
 }
