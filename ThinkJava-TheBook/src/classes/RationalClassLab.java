4c9c281fcc9c2edeaf31bd432065703247af0d08
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-11 12:26:47 +1100

    started working on rational lab

---
+++ ThinkJava-TheBook/src/classes/RationalClassLab.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class RationalClassLab {
+
+	private int numerator;
+	private int denominator;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
