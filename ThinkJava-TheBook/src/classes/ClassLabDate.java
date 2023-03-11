1d4e4786e8a37818295385be221b219d25211430
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-11 12:22:54 +1100

    date lab done

--- ThinkJava-TheBook/src/classes/ClassLabDate.java
+++ ThinkJava-TheBook/src/classes/ClassLabDate.java
@@ -1,10 +1,27 @@
 package classes;
 
+import java.time.LocalDate;
+
 public class ClassLabDate {
+	private int year;
+	private int month;
+	private int day;
+	static LocalDate now = LocalDate.now();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ClassLabDate birthday = new ClassLabDate(1998, 9, 9);
+		System.out.println(birthday.year);
+	}
 
+	public ClassLabDate() {
+		this(now.getYear(), now.getMonthValue(), now.getDayOfMonth());
+
+	}
+
+	ClassLabDate(int year, int month, int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
 	}
 
 }
