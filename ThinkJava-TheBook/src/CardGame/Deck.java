7c7c9bfcce80cc9df389008b8693fef5510358b2
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-14 23:00:43 +1100

    lengthOfTheLAstWord ddone

--- ThinkJava-TheBook/src/CardGame/Deck.java
+++ ThinkJava-TheBook/src/CardGame/Deck.java
@@ -31,4 +31,12 @@ public class Deck {
 		}
 	}
 
+	public Deck subdeck(int low, int high) {
+		Deck sub = new Deck(high - low + 1);
+		for (int i = 0; i < sub.cards.length; i++) {
+			sub.cards[i] = this.cards[low + i];
+		}
+		return sub;
+	}
+
 }
