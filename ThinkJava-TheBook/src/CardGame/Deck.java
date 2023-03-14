8369f3ecc881cbd2b43a0c4765ecde6cf865b5ae
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-14 23:01:52 +1100

    deck classs

--- ThinkJava-TheBook/src/CardGame/Deck.java
+++ ThinkJava-TheBook/src/CardGame/Deck.java
@@ -10,33 +10,6 @@ public class Deck {
 
 	private Card[] cards;
 
-	Deck(int n) {
-		this.cards = new Card[n];
-	}
-
-	Deck() {
-		this.cards = new Card[52];
-		int index = 0;
-		for (int suit = 0; suit <= 3; suit++) {
-			for (int rank = 1; rank <= 13; rank++) {
-				this.cards[index] = new Card(rank, suit);
-				index++;
-			}
-		}
-	}
-
-	public void print() {
-		for (int i = 0; i < this.cards.length; i++) {
-			System.out.println(this.cards[i]);
-		}
-	}
-
-	public Deck subdeck(int low, int high) {
-		Deck sub = new Deck(high - low + 1);
-		for (int i = 0; i < sub.cards.length; i++) {
-			sub.cards[i] = this.cards[low + i];
-		}
-		return sub;
-	}
+	
 
 }
