824c507b5c3b3519c5d016ccede3c4107897f594
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-16 23:01:58 +1100

    adding descriptions to the Player Calss

--- ThinkJava-TheBook/src/CardGame/Hand.java
+++ ThinkJava-TheBook/src/CardGame/Hand.java
@@ -1,5 +1,14 @@
 package CardGame;
 
+/*
+ * That’s it for the Deck class. Next we need a way to represent a hand, which
+ * is the col‐ lection of cards held by a player, and a pile, which is a
+ * collection of cards on the table. We could define two classes, one for hands
+ * and one for piles, but there is not much difference between them. So we’ll
+ * use one class, called Hand, for both hands and piles. Here’s what the
+ * definition looks like:
+ *
+ */
 public class Hand extends CardCollection {
 
 	public static void main(String[] args) {
@@ -15,6 +24,17 @@ public class Hand extends CardCollection {
 		System.out.printf("Draw Pile has %d cards.\n", drawPile.size());
 	}
 
+	/*
+	 * Like Deck, Hand extends CardCollection, so it inherits methods like getLabel,
+	 * size, and getCard, which are used in display. Hand also provides a
+	 * constructor, which invokes the constructor of CardCollection (and nothing
+	 * else).
+	 * 
+	 * In summary, a Deck is just like a CardCollection, but it provides a different
+	 * con‐ structor. And a Hand is just like a CardCollection, but it provides an
+	 * additional method, display.
+	 * 
+	 */
 	Hand(String label) {
 		super(label);
 	}
--- ThinkJava-TheBook/src/CardGame/Player.java
+++ ThinkJava-TheBook/src/CardGame/Player.java
@@ -4,13 +4,31 @@ public class Player {
 	private String name;
 	private Hand hand;
 
+	/*
+	 * A Player has two private attributes: a name and a hand. The constructor takes
+	 * the player’s name as a string and saves it in an instance variable. In this
+	 * example, we have to use this to distinguish between the instance variable and
+	 * the parameter with the same name.
+	 */
 	Player(String name) {
 		this.name = name;
 		this.hand = new Hand(name);
 	}
 
+	/*
+	 * The primary method that Player provides is play, which decides which card to
+	 * dis‐ card during each turn:
+	 * 
+	 * The first parameter is a reference to the Eights object that encapsulates the
+	 * state of the game. We’ll need it if we have to draw a new card. The second
+	 * parameter, prev, is the card on top of the discard pile.
+	 */
 	public Card play(Eights eights, Card prev) {
-//		Card card =
+		Card card = searchForMatch(prev);
+		if (card == null) {
+			card = drawForMatch(eights, prev);
+		}
+		return card;
 	}
 
 	public Card searchForMatch(Card prev) {
@@ -21,5 +39,11 @@ public class Player {
 
 	}
 
-	public static boolean card
+	public static boolean cardMatches(Card card1, Card card2) {
+
+	}
+
+	public int score() {
+
+	}
 }
