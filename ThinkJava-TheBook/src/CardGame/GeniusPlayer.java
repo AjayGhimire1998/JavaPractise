6b2a22e8481f790b38bba968024ec281db1cdea1
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-17 09:05:43 +1100

    overriding the player's play method

--- ThinkJava-TheBook/src/CardGame/GeniusPlayer.java
+++ ThinkJava-TheBook/src/CardGame/GeniusPlayer.java
@@ -1,9 +1,26 @@
 package CardGame;
 
+import java.util.ArrayList;
+
 public class GeniusPlayer extends Player {
 
 	GeniusPlayer(String name) {
 		super(name);
 	}
 
+	@Override
+	public Card play(Eights eights, Card prev) {
+
+	}
+
+	private ArrayList<Card> getPlayableCards(Card prev) {
+		ArrayList<Card> playableCards = new ArrayList<>();
+		for (Card card : getHand().cards) {
+			if (cardMatches(card, prev)) {
+				playableCards.add(card);
+
+			}
+		}
+		return playableCards;
+	}
 }
