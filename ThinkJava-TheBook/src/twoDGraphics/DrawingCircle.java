41ddc5ddcf935a549263cbbaeb635fcf61edcf54
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-17 08:47:14 +1100

    drew circlie

--- ThinkJava-TheBook/src/twoDGraphics/DrawingCircle.java
+++ ThinkJava-TheBook/src/twoDGraphics/DrawingCircle.java
@@ -1,10 +1,26 @@
 package twoDGraphics;
 
-public class DrawingCircle {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class DrawingCircle extends Canvas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("My Drawing");
+		Canvas canvas = new DrawingCircle();
+		canvas.setSize(400, 400);
+		frame.add(canvas);
+		frame.pack();
+		frame.setVisible(true);
 
+	}
+
+	public void paint(Graphics g) {
+		g.fillOval(100, 100, 200, 200);
+		g.setColor(Color.RED);
 	}
 
 }
