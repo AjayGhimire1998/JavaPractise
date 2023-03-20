59144603f5984edfd1f56874e5abf49ee1deadd6
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-20 20:03:51 +1100

    most of the wrapper class contructors are deprecated

---
+++ DSAinJava-TheBook/src/WrapperTypes/WrapperTypes.java
@@ -0,0 +1,11 @@
+package WrapperTypes;
+
+public class WrapperTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean isVisible = new Boolean(false);
+		System.out.println(isVisible);
+	}
+
+}
