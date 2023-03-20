e17675fb806bb43b9d4b43e879a332cd67b01857
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-20 18:41:27 +1100

    this keyword practise

--- DSAinJava-TheBook/src/firstPackage/Main.java
+++ DSAinJava-TheBook/src/firstPackage/Main.java
@@ -3,19 +3,39 @@ package firstPackage;
 public class Main {
 
 	public static void main(String[] args) {
-		String name = "Ajay";
-		String ajay = name;
-		ajay = name + "!";
+//		String name = "Ajay";
+//		String ajay = name;
+//		ajay = name + "!";
+//
+//		System.out.println(ajay);
+//		System.out.println(name);
+//
+//		name = ajay + '!';
+//		System.out.println(name);
+////		int i;
+//
+//		System.out.println(name.charAt(0));
 
-		System.out.println(ajay);
-		System.out.println(name);
+		Person jay = new Person("ajay", 24);
+		Person hehe = new Person("hehe", 25);
+		jay.printName(hehe);
 
-		name = ajay + '!';
-		System.out.println(name);
-//		int i;
+	}
 
-		System.out.println(name.charAt(0));
+}
 
+class Person {
+	String name;
+	int age;
+
+	Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void printName(Person p) {
+		System.out.println(p.name);
+		System.out.println(this.age);
 	}
 
 }
