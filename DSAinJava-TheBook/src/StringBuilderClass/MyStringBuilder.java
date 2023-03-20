44b76335f962ee936d9216d8848226918caa0d26
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-20 19:58:45 +1100

    string builder

--- DSAinJava-TheBook/src/StringBuilderClass/MyStringBuilder.java
+++ DSAinJava-TheBook/src/StringBuilderClass/MyStringBuilder.java
@@ -2,18 +2,32 @@ package StringBuilderClass;
 
 import java.util.Scanner;
 
-public class StringBuilder {
+public class MyStringBuilder {
 
 	public static void main(String[] args) {
-		StringBuilder str = new StringBuilder();
-		System.out.println(str.str);
+//		MyStringBuilder str = new MyStringBuilder();
+//		System.out.println(str.str);
+
+		String greet = "Hello";
+		char[] chars = { 'a', 'b', 'c', 'd' };
+		StringBuilder greetBuilder = new StringBuilder(greet);
+		System.out.println(greetBuilder.reverse());
+
+		StringBuilder greeting = new StringBuilder();
+
+		greeting.append("Hello World");
+
+		greeting.setCharAt(0, 'E');
+		greeting.insert(0, "YE");
+		System.out.println(greeting.toString());
+
 	}
 
 	static Scanner in;
 	static char[] charSequence;
 	String str = "";
 
-	StringBuilder() {
+	MyStringBuilder() {
 
 		System.out.println("Please enter the str upto 5 char");
 		in = new Scanner(System.in);
