c144b3a24529fc4a49ffa614086c615e212cc6a1
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-21 13:40:43 +1100

    operators in java

--- DSAinJava-TheBook/src/Enums/EnumTest.java
+++ DSAinJava-TheBook/src/Enums/EnumTest.java
@@ -5,6 +5,10 @@ public class EnumTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Day.Fri);
+
+		String name = "Ajay\b";
+		System.out.println(name);
+
 	}
 
 }
