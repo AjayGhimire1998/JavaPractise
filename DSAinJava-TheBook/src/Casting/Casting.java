2f626075dd600eae6ae66028eb7c46da805c08e5
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-22 07:46:16 +1100

    type casting

---
+++ DSAinJava-TheBook/src/Casting/Casting.java
@@ -0,0 +1,14 @@
+package Casting;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 7;
+		int j = 4;
+
+		double d = i / j;
+		System.out.println(d);
+	}
+
+}
