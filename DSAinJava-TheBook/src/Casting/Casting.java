b6d8e220b7166497ee04fc55d4fe509e1d6673b9
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-22 08:22:33 +1100

    do while loops

--- DSAinJava-TheBook/src/Casting/Casting.java
+++ DSAinJava-TheBook/src/Casting/Casting.java
@@ -1,5 +1,7 @@
 package Casting;
 
+import java.util.Scanner;
+
 public class Casting {
 
 	public static void main(String[] args) {
@@ -14,6 +16,20 @@ public class Casting {
 
 		String s = Integer.toString(i);
 		System.out.println(s + 8);
+
+		boolean isIt = true;
+		do {
+			System.out.println("Hello, it is.");
+		} while (false);
+		isIt = false;
+
+		Scanner in = new Scanner(System.in);
+
+		String input;
+		do {
+			input = in.next();
+			System.out.println(input);
+		} while (input.length() > 0);
 	}
 
 }
