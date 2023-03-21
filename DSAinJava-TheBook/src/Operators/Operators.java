4f55b8490aa002473c8e6e198fc214ccc754093c
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-21 14:53:44 +1100

    tried to recreate the unary operator method but IDK what happened

--- DSAinJava-TheBook/src/Operators/Operators.java
+++ DSAinJava-TheBook/src/Operators/Operators.java
@@ -23,11 +23,20 @@ public class Operators {
 //		int n = 9 + --i;
 //		System.out.println(n);
 
-		System.out.println(intToBinary(4));
-		System.out.println(intToHex(900));
+//		System.out.println(intToBinary(4));
+//		System.out.println(intToHex(900));
+//
+//		System.out.println(binaryToInt("100"));
 
-		System.out.println(binaryToInt("100"));
 		// Bitwise operators
+
+//		int a = 12;
+//		System.out.println(a);
+//
+//		int b = ~a;
+//		System.out.println(b);
+
+		System.out.println(unaryOperate(12));
 
 	}
 
@@ -58,5 +67,44 @@ public class Operators {
 			result = result * 2 + Character.getNumericValue(binary.charAt(i));
 		}
 		return result;
+	}
+
+	public static int[] intArrReverse(int[] nums) {
+		int[] result = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			int temp = nums[i];
+			result[i] = nums[nums.length - 1 - i];
+			result[nums.length - 1 - i] = temp;
+		}
+		return result;
+	}
+
+	public static int unaryOperate(int n) {
+		int[] binary = new int[8];
+		int i = 0;
+		while (n > 0) {
+			binary[i] = n % 2;
+			n /= 2;
+			i++;
+		}
+		int[] orderedBinary = intArrReverse(binary);
+
+		for (int j = 0; j < orderedBinary.length; j++) {
+			if (orderedBinary[j] == 0) {
+				orderedBinary[j] = 1;
+			} else {
+				orderedBinary[j] = 0;
+			}
+		}
+
+		String stringedBinary = "";
+
+		for (int k = 0; k < orderedBinary.length; k++) {
+			stringedBinary += orderedBinary[k];
+		}
+		System.out.println(stringedBinary);
+
+		return binaryToInt(stringedBinary);
+
 	}
 }
