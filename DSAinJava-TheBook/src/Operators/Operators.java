e07b0933361a1fc8daa7ae0e48949ecfef244899
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-21 14:20:23 +1100

    integer to binary

--- DSAinJava-TheBook/src/Operators/Operators.java
+++ DSAinJava-TheBook/src/Operators/Operators.java
@@ -5,23 +5,37 @@ public class Operators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i = 8;
-		System.out.println(i);
+//		int i = 8;
+//		System.out.println(i);
+//
+//		int j = i++;
+//		System.out.println(j);
+//		System.out.println(i);
+//
+//		int k = ++i;
+//		System.out.println(k);
+//		System.out.println(i);
+//
+//		int m = i--;
+//		System.out.println(m);
+//		System.out.println(i);
+//
+//		int n = 9 + --i;
+//		System.out.println(n);
 
-		int j = i++;
-		System.out.println(j);
-		System.out.println(i);
+		System.out.println(intToBinary(900));
+		// Bitwise operators
 
-		int k = ++i;
-		System.out.println(k);
-		System.out.println(i);
-
-		int m = i--;
-		System.out.println(m);
-		System.out.println(i);
-
-		int n = 9 + --i;
-		System.out.println(n);
 	}
 
+	public static String intToBinary(int n) {
+
+		String binary = "";
+
+		while (n > 0) {
+			binary = n % 2 + binary;
+			n /= 2;
+		}
+		return binary;
+	}
 }
