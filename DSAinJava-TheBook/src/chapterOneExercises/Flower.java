a15a4bdf282cd06e5866efc6563a7141438df70e
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-24 19:43:41 +1100

    flower class

---
+++ DSAinJava-TheBook/src/chapterOneExercises/Flower.java
@@ -0,0 +1,22 @@
+package chapterOneExercises;
+
+/**
+ * 
+ * Write a Java class, Flower, that has three instance variables of type String,
+ * int, and float, which respectively represent the name of the flower, its
+ * number of petals, and price. Your class must include a constructor method
+ * that initializes each variable to an appropriate value, and your class should
+ * include methods for setting the value of each type, and getting the value of
+ * each type
+ * 
+ * @author ajayghimire
+ *
+ */
+public class Flower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
