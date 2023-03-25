9c13afb8d3e9ec8366b05fae8e6f75a3ac0b1cff
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-25 21:28:03 +1100

    credit card lab

---
+++ DSAinJava-TheBook/src/chapterOneExercises/CreditCardLab.java
@@ -0,0 +1,18 @@
+package chapterOneExercises;
+
+public class CreditCardLab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private String customer;
+	private String bank;
+	private String account;
+	private int limit;
+	protected double balance;
+
+//	CreditCard(String cust, String bk, String acnt, )
+
+}
