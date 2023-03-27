23b55641dc36c63c1f0f14e1a8f32aa88a6bb078
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-27 21:06:44 +1100

    processed monthly interest and overrode charge method

--- DSAinJava-TheBook/chapterTwo/CreditCardApp/PredetoryCreditCardTwo.java
+++ DSAinJava-TheBook/chapterTwo/CreditCardApp/PredetoryCreditCardTwo.java
@@ -3,9 +3,27 @@ package CreditCardApp;
 public class PredetoryCreditCardTwo extends CreditCardTwo {
 	private double apr; // annual percentage rate
 
-	PredetoryCreditCardTwo(String customer, String bank, String account, int limit, int balance) {
-		super(customer, bank, account, limit, balance);
-		// TODO Auto-generated constructor stub
+	PredetoryCreditCardTwo(String customer, String bank, String account, int limit, int balance, double rate) {
+		super(customer, bank, account, limit, balance); // initialize superclass attributes
+		this.apr = rate;
+	}
+
+	// initialize superclass attributes
+	public void processMonth() {
+
+		if (intialBalance > 0) { // only charge interest on a positive balance
+			double monthlyFactor = Math.pow(1 + apr, 1.0 / 12); // compute monthly rate
+			intialBalance *= monthlyFactor; // assess interest
+		}
+	}
+
+	@Override // Overriding the charge method defined in the superclass
+	public boolean charge(double price) {
+		boolean isSuccess = super.charge(price); // call inherited method
+		if (!isSuccess) {
+			intialBalance += 5;
+		}
+		return isSuccess;
 	}
 
 }
