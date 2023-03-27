2f9c2d58da12d36b5cefd5cf9a209faa7c436230
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-27 21:01:27 +1100

    predetory credit card for chapter two

---
+++ DSAinJava-TheBook/chapterTwo/CreditCardApp/CreditCardTwo.java
@@ -0,0 +1,62 @@
+package CreditCardApp;
+
+public class CreditCardTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println(CardName.AMEX);
+
+	}
+
+	String customer;
+	String bank;
+	String account;
+	int limit;
+	double intialBalance;
+
+	public enum CardName {
+		VISA, MASTERCARD, AMEX
+	}
+
+	/**
+	 * Constructs a new credit card instance
+	 * 
+	 * @param customer
+	 * @param bank
+	 * @param account
+	 * @param limit
+	 * @param balance
+	 */
+	CreditCardTwo(String customer, String bank, String account, int limit, int balance) {
+		this.customer = customer;
+		this.bank = bank;
+		this.account = account;
+		this.limit = limit;
+		this.intialBalance = balance;
+
+	}
+
+	/**
+	 * Charges the given price to the card, assuming it has sufficient credit limit
+	 * 
+	 * @param price the amount to be charged
+	 * @return true if charge was accepted; false if charge was denied
+	 */
+	public boolean charge(double price) {
+		if (price + intialBalance > limit) {
+			return false;
+		}
+		intialBalance += price;
+		return true;
+	}
+
+	/**
+	 * processes customer payment that reduces balance
+	 * 
+	 * @param amount the amount of payment made
+	 */
+	public void makePayment(double amount) { // make a payment
+		intialBalance -= amount;
+	}
+
+}
---
+++ DSAinJava-TheBook/chapterTwo/CreditCardApp/PredetoryCreditCardTwo.java
@@ -0,0 +1,11 @@
+package CreditCardApp;
+
+public class PredetoryCreditCardTwo extends CreditCardTwo {
+	private double apr; // annual percentage rate
+
+	PredetoryCreditCardTwo(String customer, String bank, String account, int limit, int balance) {
+		super(customer, bank, account, limit, balance);
+		// TODO Auto-generated constructor stub
+	}
+
+}
