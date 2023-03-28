b4d2ca45caf4b2b454ed6186631dddb4421f7e59
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-28 19:54:57 +1100

    geometric progression

---
+++ DSAinJava-TheBook/chapterTwo/Progression/GeometricProgression.java
@@ -0,0 +1,29 @@
+package Progression;
+
+public class GeometricProgression extends Progression {
+	protected long base;
+
+	GeometricProgression(long b, long start) {
+		super(start);
+		base = b;
+	}
+
+	GeometricProgression(long b) {
+		this(b, 1);
+	}
+
+	GeometricProgression() {
+		this(2, 1);
+	}
+
+	@Override
+	protected void advance() {
+		current *= base;
+	}
+
+	public static void main(String[] args) {
+		GeometricProgression geo = new GeometricProgression(3, 2);
+		geo.nextValue();
+		System.out.println(geo.current);
+	}
+}
