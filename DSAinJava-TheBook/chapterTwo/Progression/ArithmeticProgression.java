ffea8cce6e1dd0ddef95aab6e333d8478fcf22a3
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-28 19:48:50 +1100

    arithmetic progression

---
+++ DSAinJava-TheBook/chapterTwo/Progression/ArithmeticProgression.java
@@ -0,0 +1,29 @@
+package Progression;
+
+public class ArithmeticProgression extends Progression {
+	protected long increment;
+
+	ArithmeticProgression() {
+		this(1, 0);
+	}
+
+	ArithmeticProgression(long stepSize) {
+		this(stepSize, 0);
+	}
+
+	ArithmeticProgression(long stepSize, long start) {
+		super(start);
+		increment = stepSize;
+	}
+
+	@Override
+	protected void advance() {
+		current += increment;
+	}
+
+	public static void main(String[] args) {
+		ArithmeticProgression arth = new ArithmeticProgression(3, 1);
+		arth.nextValue();
+		System.out.println(arth.current);
+	}
+}
