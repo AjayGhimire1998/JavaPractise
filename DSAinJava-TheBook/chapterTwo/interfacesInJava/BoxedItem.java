c78fb99580d1a451210fb847c31a1ffb486f31d5
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-29 07:24:24 +1100

    boxedItemn class for the packaging and deliver

--- DSAinJava-TheBook/chapterTwo/interfacesInJava/BoxedItem.java
+++ DSAinJava-TheBook/chapterTwo/interfacesInJava/BoxedItem.java
@@ -7,42 +7,59 @@ package interfacesInJava;
  *
  */
 public class BoxedItem implements Sellable, Transportable {
-	private String descript;
-	private int price;
-	private int weight;
-	private boolean haz;
-	private int height = 0;
-	private int width = 0;
-	private int depth = 0;
+	private String descript; // description of this item
+	private int price;// list price in cents
+	private int weight;// weight in grams
+	private boolean haz;// true if object is hazardous
+	private int height = 0;// box height in centimeters
+	private int width = 0;// box width in centimeters
+	private int depth = 0;// box depth in centimeters
+
+	BoxedItem(String desc, int p, int w, boolean h) {
+		descript = desc;
+		price = p;
+		weight = w;
+		haz = h;
+	}
 
 	@Override
 	public int weight() {
 		// TODO Auto-generated method stub
-		return 0;
+		return weight;
 	}
 
 	@Override
 	public boolean isHazardous() {
 		// TODO Auto-generated method stub
-		return false;
+		return haz;
 	}
 
 	@Override
 	public String decription() {
 		// TODO Auto-generated method stub
-		return null;
+		return descript;
 	}
 
 	@Override
 	public int listPrice() {
 		// TODO Auto-generated method stub
-		return 0;
+		return price;
 	}
 
 	@Override
 	public int lowestPrice() {
 		// TODO Auto-generated method stub
-		return 0;
+		return price / 2;
+	}
+
+	public int ensuredValue() {
+		return price * 2;
+	}
+
+	public void setBox(int h, int w, int d) {
+		height = h;
+		width = w;
+		depth = d;
 	}
 
 }
