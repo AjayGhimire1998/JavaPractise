8ae30c1573c591418fa628cb9f7f9ffe442a07f0
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-30 20:47:16 +1100

    try catch block

--- DSAinJava-TheBook/chapterTwo/exception/TryException.java
+++ DSAinJava-TheBook/chapterTwo/exception/TryException.java
@@ -4,7 +4,16 @@ public class TryException {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = -12;
+		try {
+			n = Integer.parseInt(args[0]);
+			if (n <= 0) {
+				System.out.println("n must be positive. Using default.");
+				n = 0;
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("No argument specified for n. Using default");
+		}
 	}
 
 }
