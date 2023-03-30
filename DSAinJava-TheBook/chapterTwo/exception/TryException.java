a6c2beb194c8a3f2599ce88a39955c032db97e4f
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-30 20:50:10 +1100

    try catch for main

--- DSAinJava-TheBook/chapterTwo/exception/TryException.java
+++ DSAinJava-TheBook/chapterTwo/exception/TryException.java
@@ -13,6 +13,8 @@ public class TryException {
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("No argument specified for n. Using default");
+		} catch (NumberFormatException e) {
+			System.out.println("Invalid integer argument. Usinf default");
 		}
 	}
 
