6a0c98980b42d27691b1539b7c91dac6974ca24e
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-31 16:22:37 +1100

    casting class

--- DSAinJava-TheBook/chapterTwo/CastinginJava/CastingTries.java
+++ DSAinJava-TheBook/chapterTwo/CastinginJava/CastingTries.java
@@ -5,6 +5,14 @@ public class CastingTries {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Number n;
+		Integer i;
+		n = (int) 3;
+		System.out.println(n);
+
+		i = (Integer) n;
+		System.out.println(i);
+
 	}
 
 }
