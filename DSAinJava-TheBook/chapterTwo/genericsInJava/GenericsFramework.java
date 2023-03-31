0089091544c2e2301de98fc3b0ce07c852be6cc5
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-31 19:09:43 +1100

    tested generics frameworks

--- DSAinJava-TheBook/chapterTwo/genericsInJava/GenericsFramework.java
+++ DSAinJava-TheBook/chapterTwo/genericsInJava/GenericsFramework.java
@@ -18,8 +18,15 @@ public class GenericsFramework<A, B> {
 	}
 
 	public static void main(String[] args) {
-		GenericsFramework<String, Double> gens = new GenericsFramework<>("Ajay", 24.0);
+		GenericsFramework<String, Double> gens = new GenericsFramework<String, Double>("Ajay", 24.0);
 		System.out.println(gens.getA());
 		System.out.println(gens.getB());
+
+		GenericsFramework<Integer, Character> gensTwo = new GenericsFramework<>(12, 'A');
+		System.out.println(gensTwo.getA());
+		System.out.println(gensTwo.getB());
+
+		Integer price = gensTwo.getA();
+		System.out.println(price);
 	}
 }
