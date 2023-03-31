9ded0aae28916771928698af7682bc0ba5267f50
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-01 10:28:02 +1100

    lab for credit card charge method{

--- DSAinJava-TheBook/chapterTwo/CreditCardApp/PredetoryCreditCardTwo.java
+++ DSAinJava-TheBook/chapterTwo/CreditCardApp/PredetoryCreditCardTwo.java
@@ -2,12 +2,20 @@ package CreditCardApp;
 
 public class PredetoryCreditCardTwo extends CreditCardTwo {
 	private double apr; // annual percentage rate
+	private int chargesCount;
 
 	PredetoryCreditCardTwo(String customer, String bank, String account, int limit, int balance, double rate) {
 		super(customer, bank, account, limit, balance); // initialize superclass attributes
 		this.apr = rate;
 	}
 
+	/**
+	 * The PredatoryCreditCard class provides a processMonth( ) method that models
+	 * the completion of a monthly cycle. Modify the class so that once a customer
+	 * has made ten calls to charge during a month, each additional call to that
+	 * method in the current month results in an additional $1 surcharge.
+	 *
+	 */
 	// initialize superclass attributes
 	public void processMonth() {
 
@@ -15,23 +23,42 @@ public class PredetoryCreditCardTwo extends CreditCardTwo {
 			double monthlyFactor = Math.pow(1 + apr, 1.0 / 12); // compute monthly rate
 			intialBalance *= monthlyFactor; // assess interest
 		}
+		chargesCount = 0;
 	}
 
 	@Override // Overriding the charge method defined in the superclass
+
 	public boolean charge(double price) {
 		boolean isSuccess = super.charge(price); // call inherited method
 		if (!isSuccess) {
 			intialBalance += 5;
+		} else {
+			chargesCount++;
+			if (chargesCount > 10) {
+				intialBalance += 1;
+			}
 		}
+
 		return isSuccess;
 	}
 
 	public static void main(String[] args) {
-		CreditCardTwo card = new PredetoryCreditCardTwo("Ajay", "anz", "aei0u", 100, 100, 12);
+		CreditCardTwo card = new PredetoryCreditCardTwo("Ajay", "anz", "aei0u", 90, 100, 12);
 //		card.makePayment(12);
 
 		System.out.println(card instanceof PredetoryCreditCardTwo);
 		System.out.println(card.intialBalance);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
+		card.charge(18);
 		card.charge(18);
 
 		System.out.println(card instanceof CreditCardTwo);
