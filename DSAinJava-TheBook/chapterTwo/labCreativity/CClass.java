2266c2b971eeeb2578e847afd6cd8dbd08965141
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-02 16:18:09 +1000

    testing last commit gives weird outputs

--- DSAinJava-TheBook/chapterTwo/labCreativity/CClass.java
+++ DSAinJava-TheBook/chapterTwo/labCreativity/CClass.java
@@ -8,4 +8,20 @@ public class CClass extends BClass {
 	public void setXInA(int value) {
 		super.x = value;
 	}
+
+	public static void main(String[] args) {
+		AClass aClass = new AClass();
+		BClass bClass = new BClass();
+		CClass cClass = new CClass();
+
+		System.out.println(aClass.x);
+		System.out.println(bClass.x);
+		System.out.println(cClass.x);
+
+		cClass.setXInA(12);
+
+		System.out.println(aClass.x);
+		System.out.println(bClass.x);
+		System.out.println(cClass.x);
+	}
 }
