db654ee69f981cd9d9a4188b908edef3a3c45396
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-02 17:30:35 +1000

    quadrilateral class

---
+++ DSAinJava-TheBook/chapterTwo/polygonInterfaceApp/Quadrilateral.java
@@ -0,0 +1,24 @@
+package polygonInterfaceApp;
+
+public class Quadrilateral implements Polygon {
+	private double a, b, c, d;
+
+	Quadrilateral(double a, double b, double c, double d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	@Override
+	public double area() {
+		double s = (a + b + c + d) / 2;
+		return Math.sqrt((s - a) * (s - b) * (s - c) * (s - d));
+	}
+
+	@Override
+	public double perimeter() {
+		return a + b + c + d;
+	}
+
+}
