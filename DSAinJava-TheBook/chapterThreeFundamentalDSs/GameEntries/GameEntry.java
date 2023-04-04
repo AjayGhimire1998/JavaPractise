edfe7487827d808ea75d9b9e05a80ae77c6125ac
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-05 08:39:36 +1000

    adding java doc comments

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/GameEntry.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/GameEntry.java
@@ -9,14 +9,23 @@ public class GameEntry {
 		score = i;
 	}
 
+	/**
+	 * @return name field
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return score field
+	 */
 	public int getScore() {
 		return score;
 	}
 
+	/**
+	 * @returns string representation of the entry
+	 */
 	public String toString() {
 		return "(" + name + ", " + score + ")";
 	}
