edcaaee62668f61892ea659b0580293843cdab7a
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-06 08:03:08 +1000

    practised again

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/ScoreBoard.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/ScoreBoard.java
@@ -24,22 +24,41 @@ public class ScoreBoard {
 	 * 
 	 * @param e
 	 */
+//	void add(GameEntry e) {
+//		int newScore = e.getScore();
+//		// is the new entry e really a high score?
+//		if (numOfEntries < board.length || newScore > board[numOfEntries - 1].getScore()) {
+//			if (numOfEntries < board.length) { // no score drops from the board
+//				numOfEntries++; // so overall number increases
+//			}
+//			// shift any lower scores rightward to make room for the new entry
+//			int j = numOfEntries - 1;
+//			while (j > 0 && board[j - 1].getScore() < newScore) {
+//				board[j] = board[j - 1]; // shift entry from j-1 to j
+//				j--; // and decrement j
+//			}
+//
+//			board[j] = e; // when done, add new entry
+//		}
+//	}
+
 	void add(GameEntry e) {
 		int newScore = e.getScore();
-		// is the new entry e really a high score?
-		if (numOfEntries < board.length || newScore > board[numOfEntries - 1].getScore()) {
-			if (numOfEntries < board.length) { // no score drops from the board
-				numOfEntries++; // so overall number increases
-			}
-			// shift any lower scores rightward to make room for the new entry
-			int j = numOfEntries - 1;
-			while (j > 0 && board[j - 1].getScore() < newScore) {
-				board[j] = board[j - 1]; // shift entry from j-1 to j
-				j--; // and decrement j
+		if (numOfEntries < board.length || board[numOfEntries - 1].getScore() < newScore) {
+			if (numOfEntries < board.length) {
+				numOfEntries++;
 			}
 
-			board[j] = e; // when done, add new entry
+			int j = numOfEntries - 1;
+
+			while (j > 0 && board[j - 1].getScore() < newScore) {
+				board[j] = board[j - 1];
+				j--;
+			}
+
+			board[j] = e;
 		}
+
 	}
 
 	public static void main(String[] args) {
@@ -47,6 +66,7 @@ public class ScoreBoard {
 		GameEntry gameEntry2 = new GameEntry("Arjun", 6);
 		GameEntry gameEntry3 = new GameEntry("Shiya", 10);
 		GameEntry gameEntry4 = new GameEntry("Shiyaaa", 15);
+		GameEntry gameEntry5 = new GameEntry("Ajayyyyyyyyy", 2);
 
 		ScoreBoard scoreBoard = new ScoreBoard(5);
 		scoreBoard.add(gameEntry);
@@ -60,6 +80,7 @@ public class ScoreBoard {
 		scoreBoard.add(gameEntry2);
 		scoreBoard.add(gameEntry2);
 		scoreBoard.add(gameEntry4);
+		scoreBoard.add(gameEntry5);
 		System.out.println(scoreBoard.numOfEntries);
 		System.out.println(Arrays.toString(scoreBoard.board));
 
