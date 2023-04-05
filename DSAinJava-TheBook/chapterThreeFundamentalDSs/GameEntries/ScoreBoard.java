4e5cfc53613fc1fb3504c84830464f39d836744d
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-06 07:41:31 +1000

    add method for game entry board

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/ScoreBoard.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/GameEntries/ScoreBoard.java
@@ -16,4 +16,28 @@ public class ScoreBoard {
 	ScoreBoard(int capacity) {
 		board = new GameEntry[capacity];
 	}
+
+	/**
+	 * Attempt to add a new score to the collection (if it is high enough) ∗
+	 * 
+	 * @param e
+	 */
+	void add(GameEntry e) {
+		int newScore = e.getScore();
+		// is the new entry e really a high score?
+		if (numOfEntries < board.length || newScore > board[numOfEntries - 1].getScore()) {
+			if (numOfEntries < board.length) {
+				numOfEntries++;
+			}
+
+			int j = numOfEntries - 1;
+
+			while (j > 0 && board[j - 1].getScore() < newScore) {
+				board[j] = board[j - 1];
+				j--;
+			}
+
+			board[j] = e;
+		}
+	}
 }
