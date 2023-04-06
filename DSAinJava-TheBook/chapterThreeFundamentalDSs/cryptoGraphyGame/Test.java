5d3b5c2ecf3bf087e4eaea7c03cd897bf96cc05e
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 08:24:07 +1000

    testing char and thier unicode values

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/cryptoGraphyGame/Test.java
@@ -0,0 +1,11 @@
+package cryptoGraphyGame;
+
+public class Test {
+	public static void main(String[] args) {
+		char c = 'z';
+		int j = (int) 'y';
+		System.out.println(j);
+		char y = (char) j;
+		System.out.println(y);
+	}
+}
