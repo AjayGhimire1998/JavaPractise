dcc5052328d949d66b0439d20201f5ff94827d82
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 07:57:53 +1000

    my insertion sort was not efficient

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/sortingAnArray/InsertionSort.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/sortingAnArray/InsertionSort.java
@@ -5,20 +5,37 @@ import java.util.Arrays;
 public class InsertionSort {
 
 	public static void main(String[] args) {
-		int[] nums = { 1, 6, 9, 2, 4, 9, 5, 3 };
+		int[] nums = { 1, 6, 2, 9, 4, 9, 5, 3 };
 		insertionSort(nums);
 		System.out.println(Arrays.toString(nums));
+
+		char[] chars = { 'z', 't', 'a', 'm', 'w', 'y', 'b' };
+		insertionSortFromBook(chars);
+		System.out.println(Arrays.toString(chars));
+
 	}
 
 	static void insertionSort(int[] nums) {
 		for (int i = 1; i < nums.length; i++) {
-			for (int j = 0; j < nums.length - 1; j++) {
-				if (nums[i] < nums[j]) {
-					int temp = nums[j];
-					nums[j] = nums[i];
-					nums[i] = temp;
-				}
+			int curr = nums[i];
+			int j = i;
+			while (j > 0 && nums[j - 1] > curr) {
+				nums[j] = nums[j - 1];
+				j--;
 			}
+			nums[j] = curr;
+		}
+	}
+
+	static void insertionSortFromBook(char[] chars) {
+		for (int i = 1; i < chars.length; i++) {
+			char current = chars[i];
+			int j = i;
+			while (j > 0 && chars[j - 1] > current) {
+				chars[j] = chars[j - 1];
+				j--;
+			}
+			chars[j] = current;
 		}
 	}
 }
