2f5d47f159120e15095d6c8687153cd29db4a902
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 08:59:36 +1000

    some lame two D array

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/twoDArraysAndPositionalGames/Test.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/twoDArraysAndPositionalGames/Test.java
@@ -1,7 +1,11 @@
 package twoDArraysAndPositionalGames;
 
+import java.util.Arrays;
+
 public class Test {
 	public static void main(String[] args) {
+		int[][] data = { { 1, 2, 3 }, { 4, 5, 6 } };
 
+		System.out.println(Arrays.toString(data[1]));
 	}
 }
