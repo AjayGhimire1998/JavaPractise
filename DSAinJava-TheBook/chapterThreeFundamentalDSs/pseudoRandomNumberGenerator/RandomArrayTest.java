9cc5367c3733e1f2d3d38aa1e5e245a5516dc9fa
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 08:08:18 +1000

    pseudoRandomNumber or random fron java.utils

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/pseudoRandomNumberGenerator/RandomArrayTest.java
@@ -0,0 +1,17 @@
+package pseudoRandomNumberGenerator;
+
+import java.util.Random;
+
+/**
+ * program showing some array uses
+ * 
+ * @author ajayghimire
+ *
+ */
+public class RandomArrayTest {
+	public static void main(String[] args) {
+		int[] data = new int[10];
+		Random rand = new Random();
+
+	}
+}
