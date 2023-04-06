017538720f68e5f9358ba60048d0abaf1841bbec
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 08:34:32 +1000

    contructor for the caesar cipher class

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/cryptoGraphyGame/CaesarCipher.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/cryptoGraphyGame/CaesarCipher.java
@@ -7,7 +7,19 @@ package cryptoGraphyGame;
  *
  */
 public class CaesarCipher {
-	protected char[] encoder = new char[26]; // encryption array
-	protected char[] decoder = new char[26]; // decryption array
+	private final int MAX_CODER_LENGTH = 26;
+	protected char[] encoder = new char[MAX_CODER_LENGTH]; // encryption array
+	protected char[] decoder = new char[MAX_CODER_LENGTH]; // decryption array
 
+	/**
+	 * Contructor thta initialises the ecryption and decryption arrays
+	 * 
+	 * @param rotation
+	 */
+	CaesarCipher(int rotation) {
+		for (int k = 0; k < MAX_CODER_LENGTH; k++) {
+			encoder[k] = (char) ('A' + (k + rotation) % MAX_CODER_LENGTH);
+			decoder[k] = (char) ('A' + (k - rotation + MAX_CODER_LENGTH) % MAX_CODER_LENGTH);
+		}
+	}
 }
