9a440fadf78cedb81c8e586cd228455912508c0f
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 08:44:38 +1000

    wrote transform method to return a new String with the message provided and ecrypt and decrypt methods to do the respective for the string provided

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/cryptoGraphyGame/CaesarCipher.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/cryptoGraphyGame/CaesarCipher.java
@@ -1,7 +1,8 @@
 package cryptoGraphyGame;
 
 /**
- * Class for doing encryption and decryption using the Caesar Cipher.
+ * Class for doing encryption and decryption using the Caesar Cipher. formula
+ * for pseudoRandom generator: next = (a ∗ cur + b) % n;
  * 
  * @author ajayghimire
  *
@@ -21,5 +22,43 @@ public class CaesarCipher {
 			encoder[k] = (char) ('A' + (k + rotation) % MAX_CODER_LENGTH);
 			decoder[k] = (char) ('A' + (k - rotation + MAX_CODER_LENGTH) % MAX_CODER_LENGTH);
 		}
+	}
+
+	/**
+	 * Return transformation of origin String using the given code
+	 * 
+	 * @param original
+	 * @param code
+	 * @return new String
+	 */
+	private String transform(String original, char[] code) {
+		char[] message = original.toCharArray();
+		for (int i = 0; i < message.length; i++) {
+			if (Character.isUpperCase(message[i])) {
+				int j = message[i] - 'A';
+				message[i] = code[j];
+			}
+		}
+		return new String(message);
+	}
+
+	/**
+	 * returns string represeantation for ecrypted message
+	 * 
+	 * @param message
+	 * @return
+	 */
+	public String encrypt(String message) {
+		return transform(message, encoder); // use encoder Array
+	}
+
+	/**
+	 * return string representation of decrypted secret
+	 * 
+	 * @param secret
+	 * @return
+	 */
+	public String decrypt(String secret) {
+		return transform(secret, decoder); // use decoder array
 	}
 }
