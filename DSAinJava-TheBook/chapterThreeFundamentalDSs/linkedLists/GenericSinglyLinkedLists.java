71ee3c61cf15f7f9fc1f533f8d2c38d41ef300d1
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-07 13:51:05 +1000

    node private static nested class

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/linkedLists/GenericSinglyLinkedLists.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/linkedLists/GenericSinglyLinkedLists.java
@@ -1,5 +1,25 @@
 package linkedLists;
 
-public class GenericSinglyLinkedLists {
+public class GenericSinglyLinkedLists<T> {
+	private static class Node<T> {
+		private T element;
+		private Node<T> next;
 
+		Node(T e, Node<T> n) {
+			element = e;
+			next = n;
+		}
+
+		public T getElement() {
+			return element;
+		}
+
+		public Node<T> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<T> node) {
+			next = node;
+		}
+	}
 }
