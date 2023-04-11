c532f7091c65e456903da4c10b361d1e61b29e81
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-12 06:45:44 +1000

    equivalence testing in strings

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/equivalenceTesting/TestClass.java
@@ -0,0 +1,9 @@
+package equivalenceTesting;
+
+public class TestClass {
+	public static void main(String[] args) {
+		String name = "Ajay";
+		String ajay = new String("Ajay");
+		System.out.println(name == ajay);
+	}
+}
