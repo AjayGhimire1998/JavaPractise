2bfa59d063b9d3839a4d440fc6f2ee7839e7eaaa
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-16 18:59:59 +1000

    creativity labs

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/creativityLabs/CreativityLabs.java
@@ -0,0 +1,16 @@
+package creativityLabs;
+
+public class CreativityLabs {
+
+	/**
+	 * Let A be an array of size n ≥ 2 containing integers from 1 to n−1 inclusive,
+	 * one of which is repeated. Describe an algorithm for finding the integer in A
+	 * that is repeated.
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int findRepeated(int[] nums) {
+
+	}
+}
