6dd584496aea82a0fa932df2def673b09fc04947
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-16 19:06:36 +1000

    finding repeated integer in an array

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/creativityLabs/CreativityLabs.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/creativityLabs/CreativityLabs.java
@@ -11,6 +11,18 @@ public class CreativityLabs {
 	 * @return
 	 */
 	public int findRepeated(int[] nums) {
+		if (nums.length < 2) {
+			return 0;
+		}
+		int result = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = 1; j < nums.length; j++) {
+				if (nums[i] == nums[j]) {
+					result = nums[i];
+				}
+			}
+		}
+		return result;
 
 	}
 }
