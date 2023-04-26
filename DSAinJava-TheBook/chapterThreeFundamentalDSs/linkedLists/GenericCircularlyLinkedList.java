88c5555b67e98616551ae6f69babc6729ca5fb47
AjayGhimire <ajayghimire1998@gmail.com>
2023-04-27 07:20:03 +1000

    added method question

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/linkedLists/GenericCircularlyLinkedList.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/linkedLists/GenericCircularlyLinkedList.java
@@ -181,6 +181,14 @@ public class GenericCircularlyLinkedList<T> {
 
 	}
 
+	/**
+	 * Suppose you are given two circularly linked lists, L and M. Describe an
+	 * algorithm for telling if L and M store the same sequence of elements (but
+	 * perhaps with different starting points).
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public boolean equalsWithSequence(GenericCircularlyLinkedList<T> other) {
 		if (other == this) {
 			return true;
