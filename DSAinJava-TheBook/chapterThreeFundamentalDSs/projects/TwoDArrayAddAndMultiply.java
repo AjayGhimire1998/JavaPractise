daf8caa1ed7785f21c5ffdc899afe7a88f87e47e
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-03 20:29:32 +1000

    projects for Basic DS

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/TwoDArrayAddAndMultiply.java
@@ -0,0 +1,13 @@
+package projects;
+
+public class TwoDArrayAddAndMultiply {
+	public static void main(String[] args) {
+
+		int[][] nums = { { 1, 2, 3 }, { 4, 5, 6 } };
+		int total = 0;
+		for (int i = 0; i < nums.length; i++) {
+
+		}
+	}
+
+}
