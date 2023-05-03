1aa0268452fcc193b5a85b5388d518eba6f322a8
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-03 20:32:07 +1000

    sum and product done

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/TwoDArrayAddAndMultiply.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/TwoDArrayAddAndMultiply.java
@@ -1,13 +1,26 @@
 package projects;
 
 public class TwoDArrayAddAndMultiply {
+	/**
+	 * Write a Java program for a matrix class that can add and multiply arbitrary
+	 * twodimensional arrays of integers.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		int[][] nums = { { 1, 2, 3 }, { 4, 5, 6 } };
 		int total = 0;
+		int product = 1;
 		for (int i = 0; i < nums.length; i++) {
-
+			for (int j = 0; j < nums[i].length; j++) {
+				total += nums[i][j];
+				product *= nums[i][j];
+			}
 		}
+		System.out.println("Sum: " + total);
+		System.out.println("Product: " + product);
+
 	}
 
 }
