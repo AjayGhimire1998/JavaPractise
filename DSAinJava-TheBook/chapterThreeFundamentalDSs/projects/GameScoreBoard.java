d5cacba6c23ca24a9f142bc1ef6ba37bcce43ed7
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-03 20:45:45 +1000

    using the builtin linkedlist

---
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/GameScoreBoard.java
@@ -0,0 +1,20 @@
+package projects;
+
+import java.util.LinkedList;
+
+public class GameScoreBoard {
+	private LinkedList<Integer> scoreboard;
+
+	GameScoreBoard() {
+		scoreboard = new LinkedList<>();
+	}
+
+	int getSize() {
+		return scoreboard.size();
+	}
+
+	public static void main(String[] args) {
+		GameScoreBoard game = new GameScoreBoard();
+		System.out.println(game.getSize());
+	}
+}
