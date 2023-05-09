9eb28456436b2aff9064756700e12ecc607a5fe4
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-09 17:22:42 +1000

    reworking on scoreboard class

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/GameScoreBoard.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/GameScoreBoard.java
@@ -1,69 +1,13 @@
 package projects;
 
-import java.util.LinkedList;
-
+/**
+ * Write a class that maintains the top ten scores for a game application,
+ * implementing the add and remove methods of Section 3.1.1, but using a singly
+ * linked list instead of an array.
+ * 
+ * @author ajayghimire
+ *
+ */
 public class GameScoreBoard {
-	private LinkedList<Integer> scoreboard;
 
-	GameScoreBoard() {
-		scoreboard = new LinkedList<>();
-	}
-
-	int getSize() {
-		return scoreboard.size();
-	}
-
-	void addFirst(int val) {
-		scoreboard.addFirst(val);
-	}
-
-	void addLast(int val) {
-		scoreboard.addLast(val);
-	}
-
-	void display() {
-		System.out.println(scoreboard);
-	}
-
-	Integer getFirst() {
-		return scoreboard.getFirst();
-	}
-
-	Integer getLast() {
-		return scoreboard.getLast();
-	}
-
-//	void displayInOrder() {
-//		
-//	}
-
-	public static void main(String[] args) {
-		GameScoreBoard game = new GameScoreBoard();
-		System.out.println(game.getSize());
-		game.addFirst(0);
-		game.addFirst(1);
-		game.addLast(9);
-		System.out.println(game.getSize());
-		game.display();
-		System.out.println(game.getFirst());
-
-//		GameScoreBoard game = new GameScoreBoard();
-		System.out.println(game.getSize());
-		game.addFirst(0);
-		game.addFirst(1);
-		game.addLast(9);
-		System.out.println(game.getSize());
-		game.display();
-		System.out.println(game.getFirst());
-		System.out.println(game.getSize());
-		game.addLast(9);
-		System.out.println(game.getSize());
-
-		System.out.println(game.getSize());
-
-		System.out.println(game.getSize());
-
-		System.out.println(game.getSize());
-
-	}
 }
