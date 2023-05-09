4c30b8c0b64ef2bae777415b998d915ae36a3680
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-09 17:39:41 +1000

    adding new scores upto 10 method

--- DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/GameScoreBoard.java
+++ DSAinJava-TheBook/chapterThreeFundamentalDSs/projects/GameScoreBoard.java
@@ -9,12 +9,8 @@ package projects;
  *
  */
 public class GameScoreBoard {
-	/**
-	 * private Node class for scoreboard
-	 * 
-	 * @author ajayghimire
-	 *
-	 */
+
+	// private Node class for scoreboard
 	private static class Node {
 		int score;
 		Node next;
@@ -23,5 +19,70 @@ public class GameScoreBoard {
 			this.score = score;
 			this.next = next;
 		}
+	}
+
+	// attributes of scorebaord
+	private Node head;
+	private int size;
+
+	GameScoreBoard() {
+		this.head = null;
+		this.size = 0;
+	}
+
+	void add(int score) {
+		// insert new score in order
+
+		Node newNode = new Node(score, null);
+		if (head == null) {
+			head = newNode;
+		} else if (score >= head.score) {
+			newNode.next = head;
+			head = newNode;
+		} else {
+			Node current = head;
+			while (current.next != null && score < current.next.score) {
+				current = current.next;
+			}
+			newNode.next = current.next;
+			current.next = newNode;
+		}
+
+		// remove any scores after the 10th
+		if (size >= 10) {
+			Node curr = head;
+			for (int i = 0; i < 9; i++) {
+				curr = curr.next;
+			}
+			curr.next = null;
+		} else {
+			size++;
+		}
+
+	}
+
+	public void printTopTen() {
+		Node current = head;
+		for (int i = 1; i <= size; i++) {
+			System.out.println(i + ". " + current.score);
+			current = current.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		GameScoreBoard board = new GameScoreBoard();
+		board.add(2);
+		board.add(5);
+		board.add(2);
+		board.add(10);
+		board.add(1);
+		board.add(2);
+		board.add(5);
+		board.add(2);
+		board.add(10);
+		board.add(1);
+		board.add(3);
+		board.printTopTen();
+
 	}
 }
