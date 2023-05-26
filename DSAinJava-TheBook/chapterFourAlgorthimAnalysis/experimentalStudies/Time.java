d69d028ea86b29cd078ae313e06489e1c22a5d97
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-26 19:22:55 +1000

    recording startTime to clanalyse the time required for the operation

--- DSAinJava-TheBook/chapterFourAlgorthimAnalysis/experimentalStudies/Time.java
+++ DSAinJava-TheBook/chapterFourAlgorthimAnalysis/experimentalStudies/Time.java
@@ -1,5 +1,12 @@
 package experimentalStudies;
 
 public class Time {
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
 
+		for (int i = 1; i <= 100000000; i++) {
+			System.out.println(i);
+		}
+
+	}
 }
