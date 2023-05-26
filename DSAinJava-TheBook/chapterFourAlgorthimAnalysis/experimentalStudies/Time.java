b0344fd1323a36c742327b65f0f5c4ff8276124a
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-26 19:26:42 +1000

    tested the time taken to print from 1 to 10000000, which is 8 seconds

--- DSAinJava-TheBook/chapterFourAlgorthimAnalysis/experimentalStudies/Time.java
+++ DSAinJava-TheBook/chapterFourAlgorthimAnalysis/experimentalStudies/Time.java
@@ -4,9 +4,12 @@ public class Time {
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 
-		for (int i = 1; i <= 100000000; i++) {
+		for (int i = 1; i <= 10000000; i++) {
 			System.out.println(i);
 		}
 
+		long endTime = System.currentTimeMillis();
+		long elapsed = endTime - startTime;
+		System.out.println("time taken: " + elapsed / 1000);
 	}
 }
