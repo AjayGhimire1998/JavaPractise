d06551ae77f76d98700d0a15a932d1c6f23c6c77
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-06-29 07:27:23 +1000

    enumeration demo

--- DurgaSir/CollectionFramework/cursorsInJava/EnumerationDemo.java
+++ DurgaSir/CollectionFramework/cursorsInJava/EnumerationDemo.java
@@ -1,7 +1,20 @@
 package cursorsInJava;
 
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class EnumerationDemo {
 	public static void main(String[] args) {
+		Vector<Integer> vector = new Vector<>();
 
+		for (int i = 0; i <= 10; i++) {
+			vector.addElement(i);
+		}
+		System.out.println(vector);
+
+		Enumeration<Integer> e = vector.elements();
+		while (e.hasMoreElements()) {
+			System.out.println(e.nextElement());
+		}
 	}
 }
